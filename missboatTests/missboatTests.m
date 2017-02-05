66d70cc237bea4eedb19b6e10b008624cfa857d2
Deepak <deepak@deepaks-macbook-pro.local>
2017-02-05 12:17:52 +0530

    Initial Commit

---
+++ missboatTests/missboatTests.m
@@ -0,0 +1,39 @@
+//
+//  missboatTests.m
+//  missboatTests
+//
+//  Created by Deepak on 2/5/17.
+//  Copyright © 2017 Deepak. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface missboatTests : XCTestCase
+
+@end
+
+@implementation missboatTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
